63bb8a7fd799aa12394109a76de05084f911d354
katta <katarina@earthpeople.se>
2019-11-27 17:03:54 +0100

    Add initial styling of hero, just enough to make text visible

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import styles from './styles.module.css'
+
 const Hero = ({ slice }) => (
-  <div>
-    <h1>{slice.primary.hero_title}</h1>
-    <p>{slice.primary.hero_subtitle}</p>
-    <p>{slice.primary.hero_description}</p>
-    <a href={slice.primary.hero_link_address.url}>
+  <div className={styles.hero}>
+    <h1 className={styles.title}>{slice.primary.hero_title}</h1>
+    <h2 className={styles.subTitle}>{slice.primary.hero_subtitle}</h2>
+    <p className={styles.description}>{slice.primary.hero_description}</p>
+    <a className={styles.link} href={slice.primary.hero_link_address.url}>
       {slice.primary.hero_link_title}
     </a>
   </div>
