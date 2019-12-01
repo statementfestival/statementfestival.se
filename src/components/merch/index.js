13fbc616b812b919c60f66b770ca58026e09263a
Katarina Ljungdahl <katarina@folkungagatan54.se>
2019-12-01 14:51:39 +0100

    WIP Merch component

--- src/components/merch/index.js
+++ src/components/merch/index.js
@@ -3,8 +3,25 @@ import React from 'react'
 import styles from './styles.module.css'
 
 const Merch = ({ slice }) => {
-  console.log(slice)
-  return null
+  const images = slice.fields.reduce((accumulator, current, index) => {
+    if (!current.merch_image) return accumulator
+
+    return [
+      ...accumulator,
+      {
+        url: current.merch_image.url,
+        alt: current.merch_image.alt
+      }
+    ]
+  }, [])
+  return (
+    <div className={styles.merch}>
+      <img className={styles.image} src={images[1].url} alt={images[1].alt} />
+      <div className={styles.titleContainer}>
+        <h3 className={styles.title}>Statement</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Merch
