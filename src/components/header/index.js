e153fc333543c4dc6162e1a1d91544aea77572bd
katta <katarina@earthpeople.se>
2019-12-02 15:10:45 +0100

    Show part of title in header component (according to design)

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,23 +5,33 @@ import React from 'react'
 import Logo from '../logo'
 import styles from './styles.module.css'
 
-const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
-    <Link to="/">
-      <div className={styles.logo}>
-        <Logo />
-      </div>
-      <h1 className={styles.hidden}>{siteTitle}</h1>
-    </Link>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const parts = siteTitle.split(' ')
+  return (
+    <header className={styles.header}>
+      <Link className={styles.link} to="/">
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        {parts.length === 2 ? (
+          <h1 className={styles.title}>
+            <span className={styles.hidden}>{parts[0]}</span>
+            <span className={styles.visible}>{parts[1]}</span>
+          </h1>
+        ) : (
+          <h1 className={styles.hidden}>{siteTitle}</h1>
+        )}
+      </Link>
+    </header>
+  )
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: ``
 }
 
 export default Header
