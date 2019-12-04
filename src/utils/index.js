014961da185ceda00a3a57ec10fd37562fc289d9
katta <katarina@earthpeople.se>
2019-12-04 14:56:05 +0100

    Add utility function for retreiving viewport height

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,29 @@ export const isClient = () =>
   )
 
 /**
+ * Gets viewport height
+ * @return {Number}
+ */
+export const vh = () => {
+  if (!isClient) return 0
+  return Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  )
+}
+
+export const getScrollPosition = ({ element, useWindow }) => {
+  if (!isClient) return { x: 0, y: 0 }
+
+  const target = element ? element.current : document.body
+  const position = target.getBoundingClientRect()
+
+  return useWindow
+    ? { x: window.scrollX, y: window.scrollY }
+    : { x: position.left, y: position.top }
+}
+
+/**
  * Takes a string and returns a new string that can be used as an anchor link
  * @param {string} str
  */
