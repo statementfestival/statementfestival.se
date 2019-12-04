7750b9ae733b54ac7ffb5eaed653fd50e9b5618b
hjalle <hjalle@earthpeople.se>
2019-12-04 09:18:11 +0100

    Wrap hero component with `<ImageFountain />`

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -7,22 +7,23 @@ import ImageFountain from '../image-fountain'
 import styles from './styles.module.css'
 
 const Hero = ({ slice }) => (
-  <div className={styles.hero}>
-    <ImageFountain />
-    <h1 className={styles.title}>{slice.primary.hero_title}</h1>
-    <h2 className={styles.subTitle}>{slice.primary.hero_subtitle}</h2>
-    <p className={styles.description}>{slice.primary.hero_description}</p>
-    <div className={styles.link}>
-      <ExternalLink
-        title={slice.primary.hero_link_title}
-        href={
-          slice.primary.hero_link_address
-            ? slice.primary.hero_link_address.url
-            : ''
-        }
-      />
+  <ImageFountain>
+    <div className={styles.hero}>
+      <h1 className={styles.title}>{slice.primary.hero_title}</h1>
+      <h2 className={styles.subTitle}>{slice.primary.hero_subtitle}</h2>
+      <p className={styles.description}>{slice.primary.hero_description}</p>
+      <div className={styles.link}>
+        <ExternalLink
+          title={slice.primary.hero_link_title}
+          href={
+            slice.primary.hero_link_address
+              ? slice.primary.hero_link_address.url
+              : ''
+          }
+        />
+      </div>
     </div>
-  </div>
+  </ImageFountain>
 )
 
 export default Hero
