da2e04a44322bc983111ff6b8263b454dcec67f2
hjalle <hjalle@earthpeople.se>
2019-12-04 09:19:40 +0100

    Remove document.addEventListener and use React event props instead

--- src/components/image-fountain/index.js
+++ src/components/image-fountain/index.js
@@ -1,17 +1,53 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
+import objstr from 'obj-str'
 import styles from './styles.module.css'
 
-const ImageFountain = () => {
-  const elRef = useRef()
+const images = [
+  'http://placekitten.com/1200',
+  'http://placekitten.com/1000',
+  'http://placekitten.com/800',
+  'http://placekitten.com/1400',
+  'http://placekitten.com/1300'
+]
+
+const distance = 50
+
+const ImageFountain = ({ children }) => {
+  const [visible, setVisible] = useState(null)
+  const [offset, setOffset] = useState('500px, 50px')
+  const onmousemove = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent
+    setOffset(() => {
+      return [
+        Math.ceil(offsetX / distance) * distance,
+        Math.ceil(offsetY / distance) * distance
+      ]
+    })
+  }
+
+  const [x, y] = offset
   useEffect(() => {
-    elRef.current.addEventListener('mousemove', console.log)
-    return () => {
-      elRef.current.removeEventListener('mousemove', console.log)
-    }
-  }, [elRef])
+    setVisible(state => {
+      return (state + 1) % images.length
+    })
+  }, [x, y])
+
   return (
-    <div className={styles.wrapper} ref={elRef}>
-      hejsan, vad är det för fel
+    <div className={styles.wrapper} onMouseMove={onmousemove}>
+      <div className={styles.images}>
+        {images.map((image, i) => (
+          <img
+            key={i}
+            className={objstr({
+              [styles.image]: true,
+              [styles.visible]: visible === i
+            })}
+            style={{ top: `${y}px`, left: `${x}px` }}
+            src={image}
+          />
+        ))}
+      </div>
+      <div className={styles.children}>{children}</div>
     </div>
   )
 }
