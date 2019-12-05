54cd53add16ec7673c7a33747c12422775436b69
hjalle <hjalle@earthpeople.se>
2019-12-05 14:05:27 +0100

    use mouse listener on window etc

--- src/components/image-fountain/index.js
+++ src/components/image-fountain/index.js
@@ -1,49 +1,91 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useLayoutEffect, useState } from 'react'
 import objstr from 'obj-str'
 import styles from './styles.module.css'
 
-const images = [
-  'http://placekitten.com/1200',
-  'http://placekitten.com/1000',
-  'http://placekitten.com/800',
-  'http://placekitten.com/1400',
-  'http://placekitten.com/1300'
+const urls = [
+  'http://placekitten.com/100',
+  'http://placekitten.com/110',
+  'http://placekitten.com/112',
+  'http://placekitten.com/111',
+  'http://placekitten.com/123',
+  'http://placekitten.com/200',
+  'http://placekitten.com/210',
+  'http://placekitten.com/212',
+  'http://placekitten.com/211',
+  'http://placekitten.com/323',
+  'http://placekitten.com/300',
+  'http://placekitten.com/310',
+  'http://placekitten.com/312',
+  'http://placekitten.com/311',
+  'http://placekitten.com/223'
 ]
 
 const distance = 50
 
 const ImageFountain = ({ children }) => {
+  const [images, setImages] = useState(
+    urls.map(url => ({ url, visible: false, offset: [0, 0] }))
+  )
+  const [active, setActive] = useState(false)
   const [visible, setVisible] = useState(null)
-  const [offset, setOffset] = useState('500px, 50px')
-  const onmousemove = ({ nativeEvent }) => {
-    const { offsetX, offsetY } = nativeEvent
-    setOffset(() => {
+  const [mouseOffset, setMouseOffset] = useState([0, 0])
+  const onmouseenter = () => {
+    setActive(true)
+  }
+  const onmouseleave = () => {
+    setActive(false)
+  }
+  const onmousemove = ({ pageX, pageY }) => {
+    setMouseOffset(() => {
       return [
-        Math.ceil(offsetX / distance) * distance,
-        Math.ceil(offsetY / distance) * distance
+        Math.ceil(pageX / distance) * distance,
+        Math.ceil(pageY / distance) * distance
       ]
     })
   }
 
-  const [x, y] = offset
+  useEffect(() => {
+    if (active) {
+      window.addEventListener('mousemove', onmousemove)
+      return () => {
+        window.removeEventListener('mousemove', onmousemove)
+      }
+    } else {
+      window.removeEventListener('mousemove', onmousemove)
+    }
+  }, [active])
+
   useEffect(() => {
     setVisible(state => {
       return (state + 1) % images.length
     })
-  }, [x, y])
+    setImages(state =>
+      state.map((image, i) => ({
+        ...image,
+        offset: visible === i ? [...mouseOffset] : [...image.offset],
+        visible: active && visible === i
+      }))
+    )
+  }, [...mouseOffset, active])
 
   return (
-    <div className={styles.wrapper} onMouseMove={onmousemove}>
+    <div
+      className={styles.wrapper}
+      onMouseEnter={onmouseenter}
+      onMouseLeave={onmouseleave}
+    >
       <div className={styles.images}>
-        {images.map((image, i) => (
+        {images.map(({ url, offset, visible }, i) => (
           <img
             key={i}
             className={objstr({
               [styles.image]: true,
-              [styles.visible]: visible === i
+              [styles.visible]: visible && active
             })}
-            style={{ top: `${y}px`, left: `${x}px` }}
-            src={image}
+            style={{
+              transform: `translate(calc(-50% + ${offset[0]}px), calc(-50% + ${offset[1]}px))`
+            }}
+            src={url}
           />
         ))}
       </div>
