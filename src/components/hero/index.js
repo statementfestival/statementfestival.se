0c098a9c1b2c2af79a241c9018c24899064e3c5c
katta <katarina@earthpeople.se>
2019-12-06 20:49:48 +0100

    WIP implement lottie json

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useLayoutEffect } from 'react'
+import Lottie from 'lottie-web'
 
 import ExternalLink from '../externalLink'
 
@@ -6,24 +7,45 @@ import ImageFountain from '../imageFountain'
 
 import styles from './styles.module.css'
 
-const Hero = ({ slice }) => (
-  <ImageFountain assets={slice.fields}>
-    <div className={styles.hero}>
-      <h1 className={styles.title}>{slice.primary.hero_title}</h1>
-      <h2 className={styles.subTitle}>{slice.primary.hero_subtitle}</h2>
-      <p className={styles.description}>{slice.primary.hero_description}</p>
-      <div className={styles.link}>
-        <ExternalLink
-          title={slice.primary.hero_link_title}
-          href={
-            slice.primary.hero_link_address
-              ? slice.primary.hero_link_address.url
-              : ''
-          }
-        />
+const Hero = ({ slice }) => {
+  const birdie = useRef()
+
+  useLayoutEffect(() => {
+    if (birdie.current) {
+      /*
+       * Lottie animation is loaded from static folder in order to bypass
+       * the module system: https://www.gatsbyjs.org/docs/static-folder/
+       */
+      Lottie.loadAnimation({
+        container: birdie.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '/bird.json'
+      })
+    }
+  }, [])
+
+  return (
+    <ImageFountain assets={slice.fields}>
+      <div className={styles.hero}>
+        <h1 className={styles.title}>{slice.primary.hero_title}</h1>
+        <h2 className={styles.subTitle}>{slice.primary.hero_subtitle}</h2>
+        <p className={styles.description}>{slice.primary.hero_description}</p>
+        <div className={styles.link}>
+          <ExternalLink
+            title={slice.primary.hero_link_title}
+            href={
+              slice.primary.hero_link_address
+                ? slice.primary.hero_link_address.url
+                : ''
+            }
+          />
+        </div>
+        <svg ref={birdie} />
       </div>
-    </div>
-  </ImageFountain>
-)
+    </ImageFountain>
+  )
+}
 
 export default Hero
