12f2a58ce0b13a3b4ad86b1e62214c630aeec0d6
katta <katarina@earthpeople.se>
2019-12-13 15:16:56 +0100

    Add partner logos on partner page

--- src/components/slices/imageGrid/index.js
+++ src/components/slices/imageGrid/index.js
@@ -8,18 +8,41 @@ import styles from './styles.module.css'
 const ImageGrid = ({ slice }) => {
   const hasHeadline = slice.primary && slice.primary.image_grid_title
   return (
-    <div>
+    <div className={styles.imageGrid}>
       {hasHeadline
         ? RichText.render(slice.primary.image_grid_title, linkResolver)
         : null}
-      {slice.fields.map((item, index) => {
-        const url = item.image_link ? item.image_link.url : ''
-        return (
-          <div className={styles.container} key={`ImageGrid-${index}`}>
-            <a href={url}>{url}</a>
-          </div>
-        )
-      })}
+      <div className={styles.grid}>
+        {slice.fields.map((item, index) => {
+          const external = item.image_link ? item.image_link.url : ''
+
+          const imageDesktop = item.image_grid_item ? item.image_grid_item : ''
+
+          const imageMobile =
+            item.image_grid_item && item.image_grid_item.mobile
+              ? item.image_grid_item.mobile
+              : ''
+
+          return (
+            <a
+              target="_blank"
+              className={styles.item}
+              href={external}
+              key={`ImageGrid-${index}`}
+            >
+              <img
+                className={styles.image}
+                srcSet={`${imageMobile.url} ${imageMobile.dimensions.width}w,
+            ${imageDesktop.url} ${imageDesktop.dimensions.width}w`}
+                sizes={`(max-width: 899px) 810px,
+              (min-width: 900px) 445px`}
+                src={imageDesktop.url}
+                alt={imageDesktop.alt}
+              ></img>
+            </a>
+          )
+        })}
+      </div>
     </div>
   )
 }
