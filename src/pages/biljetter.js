1c3b07ab8bdc719f6ebe60f1ae34f8d2811a0c7a
katta <katarina@earthpeople.se>
2019-12-14 15:17:47 +0100

    Add new pages

---
+++ src/pages/biljetter.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs'
+
+import Page from '../components/page'
+import Head from '../components/head'
+import SliceRenderer from '../components/sliceRenderer'
+
+export const query = graphql`
+  {
+    prismic {
+      allPages(uid: "biljetter") {
+        edges {
+          node {
+            title
+            body {
+              ... on PRISMIC_PageBodyText {
+                type
+                primary {
+                  text_content
+                  text_title
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const IntegrityPage = ({ data }) => {
+  const doc = data.prismic.allPages.edges.slice(0, 1).pop()
+  if (!doc) return null
+
+  return (
+    <Page>
+      <Head title={RichText.asText(doc.node.title)} />
+      <h1 className="visuallyHidden">{RichText.asText(doc.node.title)}</h1>
+      <SliceRenderer slices={doc.node.body} />
+    </Page>
+  )
+}
+
+export default IntegrityPage
