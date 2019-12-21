ae50ba066a2f887797f447c10bfe25a71f4d8afc
katta <katarina@earthpeople.se>
2019-12-21 16:37:11 +0100

    Add html serializer to make sure that links to Prismic documents are treated like Gatsby links

--- src/components/slices/text/index.js
+++ src/components/slices/text/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { RichText } from 'prismic-reactjs'
 
-import { linkResolver } from '../../../utils/linkResolver'
+import htmlSerializer from '../../../utils/htmlSerializer'
 import ExternalLink from '../../links/external'
 import InternalLink from '../../links/internal'
 
@@ -23,7 +23,10 @@ const Text = ({ slice }) => {
         : null}
       {slice.primary.text_content ? (
         <div className={styles.content}>
-          {RichText.render(slice.primary.text_content, linkResolver)}
+          <RichText
+            render={slice.primary.text_content}
+            htmlSerializer={htmlSerializer}
+          />
         </div>
       ) : null}
       {hasLink && linkType === 'Link.web' ? (
