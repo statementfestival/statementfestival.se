45468d9da3a75ce0d9d4cf1ab97021548ffdfb92
katta <katarina@earthpeople.se>
2020-01-08 16:26:35 +0100

    Move button styles to header since these will change based on media query

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,11 +2,11 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { RichText } from 'prismic-reactjs'
+import objstr from 'obj-str'
 
 import { linkResolver } from '../../utils/linkResolver'
 
 import Logo from '../logo'
-import ButtonLookalike from '../links/buttonLookalike'
 import styles from './styles.module.css'
 
 const Header = ({ siteTitle, menu }) => (
@@ -18,26 +18,18 @@ const Header = ({ siteTitle, menu }) => (
       <h1 className={'visuallyHidden'}>{siteTitle}</h1>
     </Link>
     <div className={styles.menu}>
-      {menu.map((item, index) => {
-        if (item.appearance === 'button') {
-          return (
-            <ButtonLookalike
-              size="small"
-              title={RichText.asText(item.link.title)}
-              to={item.link._meta}
-            />
-          )
-        }
-        return (
-          <Link
-            className={styles.menuLink}
-            key={`menuLink-${index}`}
-            to={linkResolver(item.link._meta)}
-          >
-            {RichText.asText(item.link.title)}
-          </Link>
-        )
-      })}
+      {menu.map((item, index) => (
+        <Link
+          className={objstr({
+            [styles.menuLink]: true,
+            [styles.menuButton]: item.appearance === 'button'
+          })}
+          key={`menuLink-${index}`}
+          to={linkResolver(item.link._meta)}
+        >
+          {RichText.asText(item.link.title)}
+        </Link>
+      ))}
     </div>
   </header>
 )
--- src/components/links/buttonLookalike.js
+++ src/components/links/buttonLookalike.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import objstr from 'obj-str'
 
 import { linkResolver } from '../../utils/linkResolver'
 
 import styles from './styles.module.css'
 
 /* Used when linking to internal documents */
-const ButtonLookalike = ({ title, to, size = 'regular' }) => (
-  <Link
-    className={objstr({
-      [styles.link]: true,
-      [styles[`${size}Link`]]: true
-    })}
-    to={linkResolver(to)}
-  >
+const ButtonLookalike = ({ title, to }) => (
+  <Link className={styles.link} to={linkResolver(to)}>
     {title}
   </Link>
 )
