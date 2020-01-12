e78cace15ad4744d6d5b921f32f39b4c715e521b
katta <katarina@earthpeople.se>
2020-01-12 21:07:28 +0100

    WIP embedded media slice, used on artist page

--- src/components/slices/embeddedMedia/index.js
+++ src/components/slices/embeddedMedia/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import styles from './styles.module.css'
+
 const EmbeddedMedia = ({ slice }) => {
   if (!slice.primary) return null
   // TODO: This is a work in progress, does not yet work properly on mobile
-  return <div dangerouslySetInnerHTML={{ __html: slice.primary.embed_code }} />
+  return (
+    <div
+      className={styles.container}
+      dangerouslySetInnerHTML={{ __html: slice.primary.embed_code }}
+    />
+  )
 }
 
 export default EmbeddedMedia
