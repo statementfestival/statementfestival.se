dd40eb49c5912ee55533dbfa503a10d1a5c324c9
katta <katarina@earthpeople.se>
2020-01-14 12:09:58 +0100

    Group entries by their start time

--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -16,26 +16,45 @@ const VENUE_THEMES = new Map([
 ])
 
 const Schedule = ({ entries }) => {
+  /* Construct a list with all full hours that have entries (i.e. 15:00) */
+  const hours = entries.map(item => {
+    return `${item.start_time.substring(0, 2)}:00`
+  })
+
   return (
     <div className={styles.container}>
-      {entries.map((entry, index) => (
-        <div
-          key={`entry-${index}`}
-          className={objstr({
-            [styles.entry]: true,
-            [styles[VENUE_THEMES.get(entry.venue)]]: true
-          })}
-        >
-          <p
-            className={styles.text}
-          >{`${entry.start_time} – ${entry.end_time}`}</p>
-          <Link
-            className={styles.text}
-            to={linkResolver(entry.artist._meta)}
-          >{`${RichText.asText(entry.artist.title)}`}</Link>
-          <p className={styles.text}>{entry.venue}</p>
-        </div>
-      ))}
+      {hours.map(hour => {
+        /* Filter out all entries that have a start time during current hour */
+        const currentEntries = entries.filter(
+          item => item.start_time.substring(0, 2) === hour.substring(0, 2)
+        )
+
+        return (
+          <div key={`schedule-${hour}`}>
+            <h3 className={styles.title}>{hour}</h3>
+            {currentEntries.map((entry, index) => (
+              <div
+                className={objstr({
+                  [styles.entry]: true,
+                  [styles[VENUE_THEMES.get(entry.venue)]]: true
+                })}
+              >
+                <p
+                  className={styles.text}
+                >{`${entry.start_time} – ${entry.end_time}`}</p>
+                <Link
+                  className={objstr({
+                    [styles.text]: true,
+                    [styles.link]: true
+                  })}
+                  to={linkResolver(entry.artist._meta)}
+                >{`${RichText.asText(entry.artist.title)}`}</Link>
+                <p className={styles.text}>{entry.venue}</p>
+              </div>
+            ))}
+          </div>
+        )
+      })}
     </div>
   )
 }
