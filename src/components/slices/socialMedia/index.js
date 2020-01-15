378f231b8bc75188e9b0eb2d5391d9860abbdb24
katta <katarina@earthpeople.se>
2020-01-15 13:28:25 +0100

    Make sure icon files are imported correctly (previously caused crash)

--- src/components/slices/socialMedia/index.js
+++ src/components/slices/socialMedia/index.js
@@ -1,50 +1,36 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+
+import iconFacebook from './../../../assets/facebook.svg'
+import iconInstagram from './../../../assets/instagram.svg'
+import iconSpotify from './../../../assets/spotify.svg'
 
 import styles from './styles.module.css'
 
-const SocialMedia = ({ slice }) => {
-  const data = useStaticQuery(graphql`
-    {
-      allFile(filter: { extension: { eq: "svg" } }) {
-        edges {
-          node {
-            publicURL
-          }
-        }
-      }
-    }
-  `)
-
-  return (
-    <div className={styles.container}>
-      {slice.fields.map((item, index) => {
-        let icon
-
-        for (let i = 0, len = data.allFile.edges.length; i < len; i++) {
-          const current = data.allFile.edges[i].node.publicURL
-          if (current.includes(item.icon.toLowerCase())) {
-            icon = current
-            break
-          }
-        }
-
-        /* Without icon, something is broken. Exit early */
-        if (!icon) return null
-
-        return (
-          <a
-            className={styles.link}
-            key={`socialMedia-${index}`}
-            href={item.external_link.url}
-          >
-            <span className="visuallyHidden">{item.external_link_title}</span>
-            <img src={icon} alt="" />
-          </a>
-        )
-      })}
-    </div>
-  )
+const getIcon = str => {
+  if (str === 'Facebook') return iconFacebook
+  if (str === 'Instagram') return iconInstagram
+  if (str === 'Spotify') return iconSpotify
+  return null
 }
+
+const SocialMedia = ({ slice }) => (
+  <div className={styles.container}>
+    {slice.fields.map((item, index) => {
+      const icon = getIcon(item.icon)
+      if (icon) return null
+
+      return (
+        <a
+          className={styles.link}
+          key={`socialMedia-${index}`}
+          href={item.external_link.url}
+        >
+          <span className="visuallyHidden">{item.external_link_title}</span>
+          <img src={getIcon(item.icon)} alt="" />
+        </a>
+      )
+    })}
+  </div>
+)
 
 export default SocialMedia
