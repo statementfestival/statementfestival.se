f3da012b10f612de3238f72d6eb7ca7a5ed7cbcd
katta <katarina@earthpeople.se>
2020-01-16 14:58:31 +0100

    Update default background color for consistency

--- src/components/slices/image/index.js
+++ src/components/slices/image/index.js
@@ -9,7 +9,7 @@ const Image = ({ slice }) => {
       <Img
         className={styles.image}
         alt={slice.primary.main_image.alt}
-        backgroundColor={slice.primary.main_image_color || '#eb8cb2'}
+        backgroundColor={slice.primary.main_image_color || '#954587'}
         fluid={slice.primary.main_imageSharp.childImageSharp.fluid}
       />
     </div>
