1ec15dd2e8f2e5c84c4f7be5e9ede2d4a84b89a7
katta <katarina@earthpeople.se>
2020-03-25 20:48:35 +0100

    WIP set cookie when closing cookie banner

--- src/components/cookieBanner/index.js
+++ src/components/cookieBanner/index.js
@@ -1,17 +1,30 @@
 import { Link } from 'gatsby'
-import React, { useState, useRef, useEffect } from 'react'
-import Lottie from 'lottie-web'
-import objstr from 'obj-str'
+import React from 'react'
 
 import { linkResolver } from '../../utils/linkResolver'
 import { isClient } from '../../utils'
+import closeIcon from './../../assets/x.svg'
 
-import Logo from '../logo'
 import styles from './styles.module.css'
 
 const CookieBanner = ({ description, link, linkTitle }) => {
+  const setCookie = () => {
+    /* TODO: Check if cookie is set before injecting tracking script */
+    if (
+      document.cookie
+        .split(';')
+        .some(item => item.includes('acceptedCookies=true'))
+    ) {
+      console.log('The cookie "acceptedCookies" has "true" for value')
+    }
+    document.cookie = `acceptedCookies=true; max-age=${60 * 60 * 24 * 365}`
+  }
   return (
     <div className={styles.container}>
+      <button className={styles.button} onClick={setCookie}>
+        <span className="visuallyHidden">Stäng och acceptera</span>
+        <img src={closeIcon} alt="" />
+      </button>
       <p className={styles.text}>
         {`${description} `}
         <Link className={styles.link} to={linkResolver(link._meta)}>
