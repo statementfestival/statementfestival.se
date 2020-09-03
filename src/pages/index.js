1f21b73a78bab51253896443e4ebf498062f5526
katta <katarina@earthpeople.se>
2020-09-03 11:55:00 +0200

    Update `IndexPage` to make it work with new plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,188 +1,85 @@
-// import React, { useState, useLayoutEffect } from 'react'
-// import { graphql } from 'gatsby'
-// import { RichText } from 'prismic-reactjs'
-// import nanoraf from 'nanoraf'
-
-// import { getScrollPosition, vh, getDateObject } from '../utils'
-
-// import Page from '../components/page'
-// import PageSection from '../components/pageSection'
-// import Head from '../components/head'
-// import Hero from '../components/hero'
-// import SliceRenderer from '../components/sliceRenderer'
-// import PageParallax from '../components/parallax'
-
-// export const query = graphql`
-//   {
-//     __typename
-//     prismic {
-//       allHomepages {
-//         edges {
-//           node {
-//             body {
-//               ... on PRISMIC_HomepageBodyEmbedded_media {
-//                 type
-//                 primary {
-//                   embed_code
-//                   id
-//                   caption
-//                 }
-//               }
-//               ... on PRISMIC_HomepageBodyMerch {
-//                 type
-//                 primary {
-//                   merch_link_address {
-//                     ... on PRISMIC__ExternalLink {
-//                       url
-//                     }
-//                   }
-//                   merch_link_title
-//                   merch_title
-//                   merch_link_title_hover
-//                 }
-//                 fields {
-//                   merch_image
-//                 }
-//               }
-//               ... on PRISMIC_HomepageBodyText {
-//                 type
-//                 primary {
-//                   text_content
-//                   text_title
-//                   text_link_address {
-//                     ... on PRISMIC__ExternalLink {
-//                       _linkType
-//                       url
-//                     }
-//                     ... on PRISMIC_Page {
-//                       _linkType
-//                       _meta {
-//                         uid
-//                         type
-//                       }
-//                     }
-//                   }
-//                   text_link_title
-//                 }
-//               }
-//             }
-//             title
-//             subtitle
-//             link_title
-//             link_address {
-//               ... on PRISMIC__ExternalLink {
-//                 _linkType
-//                 url
-//               }
-//               ... on PRISMIC_Page {
-//                 _linkType
-//                 _meta {
-//                   uid
-//                   type
-//                 }
-//               }
-//             }
-//             description
-//             body_details {
-//               ... on PRISMIC_HomepageBody_detailsCounter {
-//                 type
-//                 primary {
-//                   counter_date
-//                   counter_description
-//                 }
-//               }
-//               ... on PRISMIC_HomepageBody_detailsImages {
-//                 type
-//                 fields {
-//                   fountain_image
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
-// const IndexPage = ({ data }) => {
-//   const [progress, setProgress] = useState(0)
-
-//   useLayoutEffect(() => {
-//     /**
-//      * Sets progress to a value between 0 and 1 depending on how far user
-//      * has scrolled
-//      */
-//     const handleScroll = nanoraf(() => {
-//       const total = window.document.documentElement.scrollHeight
-//       const { y } = getScrollPosition({ useWindow: true })
-//       const viewportHeight = vh()
-//       setProgress(y / (total - viewportHeight))
-//     })
-
-//     window.addEventListener('scroll', handleScroll)
-//     return () => window.removeEventListener('scroll', handleScroll)
-//   })
-
-//   const doc = data.prismic.allHomepages.edges.slice(0, 1).pop()
-//   if (!doc) return null
-
-//   const title = RichText.asText(doc.node.title)
-//   const images = doc.node.body_details.find(item => item.type === 'images')
-//   const counter = doc.node.body_details.find(item => item.type === 'counter')
-
-//   /* 1. Since Prismic formats date as string i.e. '2025-01-01' */
-//   const heroData = {
-//     counter:
-//       counter && counter.primary
-//         ? {
-//             description: counter.primary.counter_description,
-//             date: counter.primary.counter_date
-//               ? getDateObject(counter.primary.counter_date)
-//               : null /* 1. */
-//           }
-//         : null,
-//     description: doc.node.description,
-//     images: images && images.fields ? images.fields : [],
-//     link_title: doc.node.link_title,
-//     link: doc.node.link_address,
-//     subtitle: RichText.asText(doc.node.subtitle),
-//     title
-//   }
-
-//   const slices = doc.node.body.map(slice => {
-//     if (slice.type === 'embedded_media') {
-//       return {
-//         ...slice,
-//         size: 'medium'
-//       }
-//     }
-//     return slice
-//   })
-
-//   return (
-//     <Page>
-//       <Head title={title} />
-//       <PageParallax progress={progress} />
-//       <PageSection size="full">
-//         <Hero {...heroData} />
-//       </PageSection>
-//       <SliceRenderer slices={slices} />
-//     </Page>
-//   )
-// }
-
-// export default IndexPage
-import React from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import { graphql } from 'gatsby'
-import Footer from '../components/footer'
+import nanoraf from 'nanoraf'
 
-const ComponentName = ({ data }) => {
+import { getScrollPosition, vh, getDateObject } from '../utils'
+
+import Page from '../components/page'
+import PageSection from '../components/pageSection'
+import Head from '../components/head'
+import Hero from '../components/hero'
+import SliceRenderer from '../components/sliceRenderer'
+import PageParallax from '../components/parallax'
+
+const IndexPage = ({ data }) => {
+  const [progress, setProgress] = useState(0)
+
+  useLayoutEffect(() => {
+    /**
+     * Sets progress to a value between 0 and 1 depending on how far user
+     * has scrolled
+     */
+    const handleScroll = nanoraf(() => {
+      const total = window.document.documentElement.scrollHeight
+      const { y } = getScrollPosition({ useWindow: true })
+      const viewportHeight = vh()
+      setProgress(y / (total - viewportHeight))
+    })
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
+  const doc = data.allPrismicHomepage.edges.slice(0, 1).pop()
+  if (!doc) return null
+
+  const { title, subtitle } = doc.node.data
+  const images = doc.node.data.body_details.find(
+    item => item.slice_type === 'images'
+  )
+
+  const counter = doc.node.data.body_details.find(
+    item => item.slice_type === 'counter'
+  )
+
+  /* 1. Since Prismic formats date as string i.e. '2025-01-01' */
+  const heroData = {
+    counter:
+      counter && counter.primary
+        ? {
+            description: counter.primary.counter_description,
+            date: counter.primary.counter_date
+              ? getDateObject(counter.primary.counter_date)
+              : null /* 1. */
+          }
+        : null,
+    description: doc.node.data.description,
+    images: images && images.items ? images.items : [],
+    link_title: doc.node.data.link_title,
+    link: doc.node.data.link_address,
+    subtitle: subtitle.text,
+    title: title.text
+  }
+
+  const slices = doc.node.data.body.map(slice => {
+    if (slice.slice_type === 'embedded_media') {
+      return {
+        ...slice,
+        size: 'medium'
+      }
+    }
+    return slice
+  })
+
   return (
-    <>
-      <pre>{JSON.stringify(data, null, 4)}</pre>
-      <Footer />
-    </>
+    <Page>
+      <Head title={title.text} />
+      <PageParallax progress={progress} />
+      <PageSection size="full">
+        <Hero {...heroData} />
+      </PageSection>
+      <SliceRenderer slices={slices} />
+    </Page>
   )
 }
 
@@ -192,10 +89,76 @@ export const query = graphql`
       edges {
         node {
           data {
+            body {
+              ... on PrismicHomepageBodyEmbeddedMedia {
+                slice_type
+                primary {
+                  caption
+                  embed_code
+                  id
+                }
+              }
+              ... on PrismicHomepageBodyText {
+                slice_type
+                primary {
+                  text_content {
+                    raw
+                  }
+                  text_title {
+                    raw
+                  }
+                  text_link_title
+                  text_link_address {
+                    type
+                    url
+                  }
+                }
+              }
+              ... on PrismicHomepageBodyMerch {
+                id
+                slice_type
+                primary {
+                  merch_link_address {
+                    uid
+                    type
+                  }
+                  merch_link_title
+                  merch_link_title_hover
+                  merch_title
+                }
+              }
+            }
             title {
-              html
               text
-              raw
+            }
+            subtitle {
+              text
+            }
+            link_title
+            link_address {
+              link_type
+              url
+              uid
+              type
+            }
+            description
+            body_details {
+              ... on PrismicHomepageBodyDetailsCounter {
+                slice_type
+                primary {
+                  counter_date
+                  counter_description
+                }
+              }
+              ... on PrismicHomepageBodyDetailsImages {
+                slice_type
+                items {
+                  fountain_image {
+                    alt
+                    url
+                  }
+                }
+              }
             }
           }
         }
@@ -204,4 +167,4 @@ export const query = graphql`
   }
 `
 
-export default ComponentName
+export default IndexPage
