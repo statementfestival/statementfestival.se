30928c9ce455e3e9117d7acc6dc1cada39776dc0
katta <katarina@earthpeople.se>
2020-09-03 10:23:57 +0200

    Update `404` page's graphql query in order to make it work with new plugin

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,32 +1,30 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { RichText } from 'prismic-reactjs'
 
 import Page from '../components/page'
 import Head from '../components/head'
 import SliceRenderer from '../components/sliceRenderer'
 
 const NotFoundPage = ({ data }) => {
-  return <pre>{JSON.stringify(data, null, 4)}</pre>
-  // const doc = data.prismic.allWebsites.edges.slice(0, 1).pop()
-  // if (!doc) return null
+  const doc = data.allPrismicWebsite.edges.slice(0, 1).pop()
+  if (!doc) return null
 
-  // return (
-  //   <Page>
-  //     <Head title={RichText.asText(doc.node.not_found_title)} />
-  //     <SliceRenderer
-  //       slices={[
-  //         {
-  //           type: 'text',
-  //           primary: {
-  //             text_title: doc.node.not_found_title,
-  //             text_content: doc.node.not_found_description
-  //           }
-  //         }
-  //       ]}
-  //     />
-  //   </Page>
-  // )
+  return (
+    <Page>
+      <Head title={doc.node.data.not_found_title.text} />
+      <SliceRenderer
+        slices={[
+          {
+            type: 'text',
+            primary: {
+              text_title: doc.node.data.not_found_title.raw,
+              text_content: doc.node.data.not_found_description.raw
+            }
+          }
+        ]}
+      />
+    </Page>
+  )
 }
 
 export const query = graphql`
@@ -37,10 +35,10 @@ export const query = graphql`
           id
           data {
             not_found_description {
-              html
+              raw
             }
             not_found_title {
-              html
+              raw
             }
           }
         }
