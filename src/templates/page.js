142a2b837d84441972a9de5a6b9ae29084a96b17
katta <katarina@earthpeople.se>
2020-09-03 14:45:58 +0200

    Update `page` template to make it compatible with new plugin

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,132 +1,43 @@
-// import React from 'react'
-// import { graphql } from 'gatsby'
-// import { RichText } from 'prismic-reactjs'
-
-// import Page from '../components/page'
-// import PageSection from '../components/pageSection'
-// import Head from '../components/head'
-// import SliceRenderer from '../components/sliceRenderer'
-
-// export const query = graphql`
-//   query PageQuery($uid: String) {
-//     prismic {
-//       allPages(uid: $uid) {
-//         edges {
-//           node {
-//             title
-//             body {
-//               ... on PRISMIC_PageBodyText {
-//                 type
-//                 primary {
-//                   text_content
-//                   text_title
-//                 }
-//               }
-//               ... on PRISMIC_PageBodyContact_group {
-//                 fields {
-//                   description
-//                   email_address
-//                 }
-//                 type
-//               }
-//               ... on PRISMIC_PageBodyImage_grid {
-//                 type
-//                 primary {
-//                   image_grid_title
-//                 }
-//                 fields {
-//                   image
-//                   imageSharp {
-//                     childImageSharp {
-//                       fluid(quality: 100, maxWidth: 851) {
-//                         ...GatsbyImageSharpFluid_noBase64
-//                       }
-//                     }
-//                   }
-//                   image_link {
-//                     ... on PRISMIC__ExternalLink {
-//                       url
-//                     }
-//                   }
-//                 }
-//               }
-//               ... on PRISMIC_PageBodyFaq {
-//                 fields {
-//                   faq_question
-//                   faq_answer
-//                 }
-//                 primary {
-//                   faq_title
-//                 }
-//                 type
-//               }
-//               ... on PRISMIC_PageBodyForm {
-//                 type
-//                 primary {
-//                   form_address {
-//                     ... on PRISMIC__ExternalLink {
-//                       url
-//                     }
-//                   }
-//                   form_description
-//                   form_success_description
-//                   form_success_title
-//                   form_title
-//                   form_type
-//                   form_disclaimer
-//                 }
-//               }
-//             }
-//             meta_description
-//             og_image
-//             _meta {
-//               uid
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
-// const PAGE_WITH_VISIBLE_TITLE = ['biljetter', 'rekrytering']
-
-// const SinglePage = ({ data }) => {
-//   const doc = data.prismic.allPages.edges.slice(0, 1).pop()
-//   if (!doc) return null
-
-//   const renderTitleVisually = PAGE_WITH_VISIBLE_TITLE.some(
-//     item => item === doc.node._meta.uid
-//   )
-
-//   return (
-//     <Page>
-//       <Head
-//         title={RichText.asText(doc.node.title)}
-//         description={doc.node.meta_description}
-//         image={doc.node.og_image ? doc.node.og_image.url : null}
-//       />
-//       {renderTitleVisually ? (
-//         <PageSection>
-//           <h1>{RichText.asText(doc.node.title)}</h1>
-//         </PageSection>
-//       ) : (
-//         <h1 className="visuallyHidden">{RichText.asText(doc.node.title)}</h1>
-//       )}
-//       <SliceRenderer slices={doc.node.body} />
-//     </Page>
-//   )
-// }
-
-// export default SinglePage
 import React from 'react'
 import { graphql } from 'gatsby'
 
-const SinglePage = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+import Page from '../components/page'
+import PageSection from '../components/pageSection'
+import Head from '../components/head'
+import SliceRenderer from '../components/sliceRenderer'
+
+const PAGE_WITH_VISIBLE_TITLE = ['biljetter', 'rekrytering']
+
+const SinglePage = ({ data }) => {
+  const doc = data.allPrismicPage.edges.slice(0, 1).pop()
+  if (!doc) return null
+
+  const renderTitleVisually = PAGE_WITH_VISIBLE_TITLE.some(
+    item => item === doc.node.uid
+  )
+
+  return (
+    <Page>
+      <Head
+        title={doc.node.data.title.text}
+        description={doc.node.data.meta_description}
+        image={doc.node.data.og_image ? doc.node.data.og_image.url : null}
+      />
+      {renderTitleVisually ? (
+        <PageSection>
+          <h1>{doc.node.data.title.text}</h1>
+        </PageSection>
+      ) : (
+        <h1 className="visuallyHidden">{doc.node.data.title.text}</h1>
+      )}
+      <SliceRenderer slices={doc.node.data.body} />
+    </Page>
+  )
+}
 
 export const query = graphql`
-  {
-    allPrismicPage {
+  query($uid: String) {
+    allPrismicPage(filter: { uid: { eq: $uid } }) {
       nodes {
         uid
       }
@@ -143,10 +54,10 @@ export const query = graphql`
                 slice_type
                 primary {
                   text_content {
-                    html
+                    raw
                   }
                   text_title {
-                    html
+                    raw
                   }
                 }
               }
@@ -162,7 +73,7 @@ export const query = graphql`
                 slice_type
                 primary {
                   image_grid_title {
-                    html
+                    raw
                   }
                 }
                 items {
@@ -182,7 +93,7 @@ export const query = graphql`
                 id
                 items {
                   faq_answer {
-                    html
+                    raw
                   }
                   faq_question
                 }
@@ -196,19 +107,19 @@ export const query = graphql`
                     url
                   }
                   form_description {
-                    html
+                    raw
                   }
                   form_disclaimer {
-                    html
+                    raw
                   }
                   form_success_description {
-                    html
+                    raw
                   }
                   form_success_title {
-                    html
+                    raw
                   }
                   form_title {
-                    html
+                    raw
                   }
                   form_type
                 }
