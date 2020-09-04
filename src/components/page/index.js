baac754cd4821ab46e06469902cb36093fbb86ca
katta <katarina@earthpeople.se>
2020-09-04 09:59:14 +0200

    Re-add StaticQuery to retrieve site title from cms

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import objstr from 'obj-str'
 
 import Header from '../header'
@@ -9,67 +9,28 @@ import PageSection from '../pageSection'
 
 import styles from './styles.module.css'
 
-/**
- * Currently, StaticQuery warns about invalid prop type, but this is due to an
- * [open issue](https://github.com/birkir/gatsby-source-prismic-graphql/issues/101)
- * in `gatsby-source-prismic-graphql` plugin. It can safely be ignored.
- *
- * The plugin does not yet support useStaticQuery hook.
- * More information can be found here:
- * [gatsby-source-prismic-graphql](https://www.gatsbyjs.org/packages/gatsby-source-prismic-graphql/#usestaticquery).
- */
-// const Page = ({ children, type = 'regular' }) => {
-//   return (
-//     <StaticQuery
-//       query={graphql`
-//         query {
-//           prismic {
-//             allWebsites {
-//               edges {
-//                 node {
-//                   meta_description
-//                   site_title
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `}
-//       render={data => {
-//         const doc = data.prismic.allWebsites.edges.slice(0, 1).pop()
-//         if (!doc) return null
-
-//         return (
-//           <div className={styles.page} id="main">
-//             <PageSection size="full">
-//               <Header siteTitle={doc.node.site_title} />
-//             </PageSection>
-//             <main
-//               className={objstr({
-//                 [styles.main]: true,
-//                 [styles.regular]: type === 'regular',
-//                 [styles.artist]: type === 'artist'
-//               })}
-//             >
-//               {children}
-//             </main>
-//             <div className={styles.footer}>
-//               <PageSection>
-//                 <Footer />
-//               </PageSection>
-//             </div>
-//           </div>
-//         )
-//       }}
-//     />
-//   )
-// }
-
 const Page = ({ children, type = 'regular' }) => {
+  const data = useStaticQuery(graphql`
+    {
+      allPrismicWebsite {
+        edges {
+          node {
+            data {
+              site_title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const doc = data.allPrismicWebsite.edges.slice(0, 1).pop()
+  if (!doc) return null
+
   return (
     <div className={styles.page} id="main">
       <PageSection size="full">
-        <Header siteTitle={'SiteTitle'} />
+        <Header siteTitle={doc.node.data.site_title} />
       </PageSection>
       <main
         className={objstr({
