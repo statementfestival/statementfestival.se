84a55633e619b3f3ef74dadccf09c285ffce2310
katta <katarina@earthpeople.se>
2020-11-07 17:37:48 +0100

    Add basic file input component

---
+++ src/components/fileInput/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Error from '../error'
+
+import styles from './styles.module.css'
+
+const FileInput = ({
+  type,
+  id,
+  name,
+  autoComplete,
+  value,
+  onChange,
+  label,
+  error,
+  accept
+}) => (
+  <div className={styles.container}>
+    <label className={styles.label} htmlFor={id}>
+      <span className={styles.text}>{label}</span>
+      <input
+        className={styles.input}
+        type={type}
+        id={id}
+        name={name}
+        onChange={onChange}
+        accept={accept}
+      />
+    </label>
+    {error ? <Error message={error} /> : null}
+  </div>
+)
+
+export default FileInput
